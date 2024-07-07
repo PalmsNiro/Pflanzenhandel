b661015f4690313eddc081c3d65afd9a6ad545fb
PalmsNiro <sebi.herr13@gmail.com>
2024-07-08 01:17:32 +0200

    Leaderboard kann jetzt sortiert werden nach Quests Completed

--- src/main/java/com/example/pflanzenhandel/controller/LeaderboardController.java
+++ src/main/java/com/example/pflanzenhandel/controller/LeaderboardController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.pflanzenhandel.entity.*;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.management.Query;
 import java.security.Principal;
@@ -19,11 +20,18 @@ public class LeaderboardController {
     private BenutzerRepository userRepository;
 
     @GetMapping({"/leaderboard"})
-    public String showUsersOnLeaderboard(Query query, Model model, Principal principal) {
-        List<Benutzer> users;
-        users = userRepository.findAll();
-        // user in absteigender reihenfolge sortieren
-        users.sort((u1, u2) -> Integer.compare(u2.getLevel(), u1.getLevel()));
+    public String showUsersOnLeaderboard(@RequestParam(value = "sort", required = false) String sort, Model model, Principal principal) {
+        List<Benutzer> users = userRepository.findAll();
+
+        // Sortiere die Benutzer nach dem angegebenen Kriterium
+        if ("level".equals(sort)) {
+            users.sort((u1, u2) -> Integer.compare(u2.getLevel(), u1.getLevel()));
+        } else if ("questsCompleted".equals(sort)) {
+            users.sort((u1, u2) -> Integer.compare(u2.getNumberOfQuestsCompleted(), u1.getNumberOfQuestsCompleted()));
+        } else {
+            // Standard-Sortierung (z.B. nach Level)
+            users.sort((u1, u2) -> Integer.compare(u2.getLevel(), u1.getLevel()));
+        }
 
         model.addAttribute("users", users);
 

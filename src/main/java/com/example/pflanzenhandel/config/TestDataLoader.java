d52825148310aa364ef8249a4db7353bf871a599
YER15 <132906415+YER15@users.noreply.github.com>
2024-07-10 22:49:38 +0200

    Update TestDataLoader.java

--- src/main/java/com/example/pflanzenhandel/config/TestDataLoader.java
+++ src/main/java/com/example/pflanzenhandel/config/TestDataLoader.java
@@ -56,13 +56,13 @@ public class TestDataLoader implements ApplicationListener<ContextRefreshedEvent
         Quest quest3 = new Quest();
         quest3.setDescription("Kaufe eine Pflanze.");
         quest3.setNeededAmount(1);
-        quest3.setXpForUser(5);
+        quest3.setXpForUser(2);
         questService.save(quest3);
 
         //Funktional
         Quest quest4 = new Quest();
-        quest4.setDescription("Favorisiere 10 Pflanzen");
-        quest4.setNeededAmount(10);
+        quest4.setDescription("Favorisiere 5 Pflanzen");
+        quest4.setNeededAmount(5);
         quest4.setXpForUser(1);
         questService.save(quest4);
 
@@ -88,16 +88,16 @@ public class TestDataLoader implements ApplicationListener<ContextRefreshedEvent
         questService.save(quest7);
 
         Quest questForTest2 = new Quest();
-        questForTest2.setDescription("Schreibe 1 Nachricht");
-        questForTest2.setNeededAmount(1);
-        questForTest2.setXpForUser(3);
+        questForTest2.setDescription("Schreibe 2 Nachrichten");
+        questForTest2.setNeededAmount(2);
+        questForTest2.setXpForUser(1);
         questService.save(questForTest2);
 
         //Funktional
         Quest questForTest = new Quest();
-        questForTest.setDescription("Erhalte insgesammt 3 Erfahrungs Punkte.");
-        questForTest.setNeededAmount(3);
-        questForTest.setXpForUser(10);
+        questForTest.setDescription("Erhalte insgesammt 5 Erfahrungs Punkte.");
+        questForTest.setNeededAmount(5);
+        questForTest.setXpForUser(1);
         questService.save(questForTest);
 
 
@@ -120,7 +120,7 @@ public class TestDataLoader implements ApplicationListener<ContextRefreshedEvent
         normalUser.setPassword("1234");
         normalUser.setRoles(userRolles);
         normalUser.setExperiencePoints(5);
-        normalUser.setLevel(2);
+        normalUser.setLevel(1);
         userService.saveUser(normalUser);
 
         if (normalUser.getId() == null) {
@@ -146,7 +146,7 @@ public class TestDataLoader implements ApplicationListener<ContextRefreshedEvent
         debbyQuest4.setUser(debbyUser);
         debbyQuest4.setQuest(quest4);
         debbyQuest5.setUser(debbyUser);
-        debbyQuest5.setQuest(quest3);
+        debbyQuest5.setQuest(quest7);
 
 
         HashSet<UserQuest> debbyUserQuest = new HashSet<UserQuest>();
@@ -157,14 +157,13 @@ public class TestDataLoader implements ApplicationListener<ContextRefreshedEvent
         debbyUserQuest.add(debbyQuest5);
 
 
-
         debbyUser.setUsername("debby");
         debbyUser.setPassword("1234");
         debbyUser.setFirstName("Debby");
         debbyUser.setLastName("Arueyingho");
         debbyUser.setEmail("debby.arueyingho@gmail.com");
-        debbyUser.setLevel(2);
-        debbyUser.setExperiencePoints(9);
+        debbyUser.setLevel(5);
+        debbyUser.setExperiencePoints(8);
         debbyUser.setRoles(userRolles);
         debbyUser.setNumberOfQuestsCompleted(2);
         debbyUser.setUserQuests(debbyUserQuest);
@@ -181,7 +180,7 @@ public class TestDataLoader implements ApplicationListener<ContextRefreshedEvent
         admin.setUsername("admin");
         admin.setPassword("admin");
         admin.setExperiencePoints(9);
-        admin.setLevel(9);
+        admin.setLevel(1);
         admin.setRoles(adminRolles);
         userService.saveUser(admin);
 
@@ -191,6 +190,66 @@ public class TestDataLoader implements ApplicationListener<ContextRefreshedEvent
             logger.info("Benutzer 'admin' erfolgreich gespeichert mit ID: " + admin.getId());
         }
 
+        Benutzer isikUser = new Benutzer();
+        isikUser.setUsername("isik");
+        isikUser.setPassword("1234");
+        isikUser.setRoles(userRolles);
+        isikUser.setExperiencePoints(8);
+        isikUser.setLevel(3);
+        isikUser.setNumberOfQuestsCompleted(2);
+        userService.saveUser(isikUser);
+
+        if (isikUser.getId() == null) {
+            logger.error("Fehler beim Speichern des Benutzers 'user'");
+        } else {
+            logger.info("Benutzer 'user' erfolgreich gespeichert mit ID: " + isikUser.getId());
+        }
+
+        Benutzer bastiUser = new Benutzer();
+        bastiUser.setUsername("basti");
+        bastiUser.setPassword("1234");
+        bastiUser.setRoles(userRolles);
+        bastiUser.setExperiencePoints(8);
+        bastiUser.setLevel(6);
+        bastiUser.setNumberOfQuestsCompleted(5);
+        userService.saveUser(bastiUser);
+
+        if (bastiUser.getId() == null) {
+            logger.error("Fehler beim Speichern des Benutzers 'user'");
+        } else {
+            logger.info("Benutzer 'user' erfolgreich gespeichert mit ID: " + bastiUser.getId());
+        }
+
+        Benutzer yannicUser = new Benutzer();
+        yannicUser.setUsername("yannic");
+        yannicUser.setPassword("1234");
+        yannicUser.setRoles(userRolles);
+        yannicUser.setExperiencePoints(8);
+        yannicUser.setLevel(4);
+        yannicUser.setNumberOfQuestsCompleted(6);
+        userService.saveUser(yannicUser);
+
+        if (yannicUser.getId() == null) {
+            logger.error("Fehler beim Speichern des Benutzers 'user'");
+        } else {
+            logger.info("Benutzer 'user' erfolgreich gespeichert mit ID: " + yannicUser.getId());
+        }
+
+        Benutzer phillipUser = new Benutzer();
+        phillipUser.setUsername("phillip");
+        phillipUser.setPassword("1234");
+        phillipUser.setRoles(userRolles);
+        phillipUser.setExperiencePoints(8);
+        phillipUser.setLevel(5);
+        phillipUser.setNumberOfQuestsCompleted(4);
+        userService.saveUser(phillipUser);
+
+        if (phillipUser.getId() == null) {
+            logger.error("Fehler beim Speichern des Benutzers 'user'");
+        } else {
+            logger.info("Benutzer 'user' erfolgreich gespeichert mit ID: " + phillipUser.getId());
+        }
+
         /*-------------------------------------------------Products----------------------------------------------*/
 
         Product product1 = new Product();
@@ -198,9 +257,8 @@ public class TestDataLoader implements ApplicationListener<ContextRefreshedEvent
         product1.setPrice(6.99);
         product1.setHeight(40);
         product1.setOverPot(false);
-        product1.setShippingCosts(5.50);
         product1.setDescription("Die Pflanze wurde regelmäßig gegossen und ist in einem guten Zustand. Bei Fragen gerne eine Nachricht an mich.");
-        product1.setVerkaufer(debbyUser);
+        product1.setVerkaufer(yannicUser);
         product1.setImageUrls(Arrays.asList("https://eastgate.megapaints.co.za/wp-content/uploads/671826-1.jpg"));
         product1.setMainImageUrl("https://eastgate.megapaints.co.za/wp-content/uploads/671826-1.jpg");
         productService.saveProduct(product1);
@@ -210,7 +268,6 @@ public class TestDataLoader implements ApplicationListener<ContextRefreshedEvent
         product6.setPrice(25.99);
         product6.setHeight(80);
         product6.setOverPot(true);
-        product6.setShippingCosts(0.00);
         product6.setDescription("Wunderschöner Blauregen, winterfest. Nur Abhohlung");
         product6.setImageUrls(Arrays.asList("https://www.poetschke.de/out/pictures/master/product/1/generated/560__/206586-00-GPIE.jpg.webp"));
         product6.setVerkaufer(debbyUser);
@@ -222,9 +279,8 @@ public class TestDataLoader implements ApplicationListener<ContextRefreshedEvent
         product2.setPrice(65.99);
         product2.setHeight(60);
         product2.setOverPot(false);
-        product2.setShippingCosts(5.50);
         product2.setDescription("Großer Pachira aquatica Baum in gutem Zustand. Perfekt für Ihr Zuhause oder Büro.");
-        product2.setVerkaufer(normalUser);
+        product2.setVerkaufer(bastiUser);
         product2.setImageUrls(Arrays.asList("https://cf.ltkcdn.net/feng-shui/images/std-xs/270900-340x340-money-tree-feng-shui.jpg"));
         product2.setMainImageUrl("https://cf.ltkcdn.net/feng-shui/images/std-xs/270900-340x340-money-tree-feng-shui.jpg");
         productService.saveProduct(product2);
@@ -234,7 +290,6 @@ public class TestDataLoader implements ApplicationListener<ContextRefreshedEvent
         product7.setPrice(25.99);
         product7.setHeight(80);
         product7.setOverPot(true);
-        product7.setShippingCosts(2.00);
         product7.setDescription("Gut duftender Lavendel, stets gut gepflegt.");
         product7.setImageUrls(Arrays.asList("https://garten-von-ehren.imgbolt.de/thumbnail/56/38/68/1675700630/lavandula-angustifolia_1920x1920.jpg"));
         product7.setVerkaufer(debbyUser);
@@ -246,9 +301,8 @@ public class TestDataLoader implements ApplicationListener<ContextRefreshedEvent
         product3.setPrice(30.00);
         product3.setHeight(150);
         product3.setOverPot(false);
-        product3.setShippingCosts(5.50);
         product3.setDescription("Schöne Palme, ideal für den Innenbereich. Bei Rückfragen stehe ich per Nachricht zur Verfügung.");
-        product3.setVerkaufer(debbyUser);
+        product3.setVerkaufer(isikUser);
         product3.setImageUrls(Arrays.asList("https://img.kleinanzeigen.de/api/v1/prod-ads/images/78/78ce54d4-f1b0-48c4-9299-325d63c9d880?rule=$_59.JPG"));
         product3.setMainImageUrl("https://img.kleinanzeigen.de/api/v1/prod-ads/images/78/78ce54d4-f1b0-48c4-9299-325d63c9d880?rule=$_59.JPG");
         productService.saveProduct(product3);
@@ -258,7 +312,6 @@ public class TestDataLoader implements ApplicationListener<ContextRefreshedEvent
         product4.setPrice(10.00);
         product4.setHeight(90);
         product4.setOverPot(true);
-        product4.setShippingCosts(5.50);
         product4.setDescription("Wunderschöne Orchidee mit lila Blüten. Mein Übertopf ist im Preis natürlich enthalten.");
         product4.setVerkaufer(debbyUser);
         product4.setImageUrls(Arrays.asList("https://cdn.mdr.de/mdr-garten/pflanzen/phalaenopsis-orchideen-zimmerpflanze-bluete-102_v-variantBig1x1_w-1280_zc-3061602c.jpg?version=58136", "https://cdn.pflanzen-koelle.de/media/26/20/17/1683680685/0215152627-Phalaenopsis-Midi-Dark-Pink-2R-12cmT-H65cm2_124057.jpg?width=520&quality=80"));
@@ -270,10 +323,9 @@ public class TestDataLoader implements ApplicationListener<ContextRefreshedEvent
         product5.setPrice(80.00);
         product5.setHeight(50);
         product5.setOverPot(true);
-        product5.setShippingCosts(5.50);
         product5.setDescription("Regelmäßig gepflegter Bonsai Baum, perfekt fürs Büro.");
         product5.setImageUrls(Arrays.asList("https://www.bonsai.saarland/images/product_images/info_images/DSC_0262.JPG", "https://www.bonsai.saarland/images/product_images/info_images/DSC_0263.JPG"));
-        product5.setVerkaufer(normalUser);
+        product5.setVerkaufer(bastiUser);
         product5.setMainImageUrl("https://www.bonsai.saarland/images/product_images/info_images/DSC_0263.JPG");
         productService.saveProduct(product5);
 
@@ -282,10 +334,9 @@ public class TestDataLoader implements ApplicationListener<ContextRefreshedEvent
         product8.setPrice(8.99);
         product8.setHeight(30);
         product8.setOverPot(false);
-        product8.setShippingCosts(3.99);
         product8.setDescription("Gut gewachsener Schnittlauch. Muss leider wegen Platzmangel meinerseits verkauft werden.");
         product8.setImageUrls(Arrays.asList("https://www.gwenys-haus.de/media/image/product/5262/lg/kunstpflanze-schnittlauch-at-topf.webp"));
-        product8.setVerkaufer(admin);
+        product8.setVerkaufer(phillipUser);
         product8.setMainImageUrl("https://www.gwenys-haus.de/media/image/product/5262/lg/kunstpflanze-schnittlauch-at-topf.webp");
         productService.saveProduct(product8);
 
